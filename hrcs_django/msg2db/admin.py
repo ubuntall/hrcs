1db709b711b3c8457cb6fac91d4449ea9b57a9e1
gaojinhua <26028188@qq.com>
2018-10-21 11:01:34 +0800

    修改后台显示列

--- hrcs_django/msg2db/admin.py
+++ hrcs_django/msg2db/admin.py
@@ -2,4 +2,31 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Msg
-admin.site.register(Msg)
+
+
+@admin.register(Msg)
+class MsgAdmin(admin.ModelAdmin):
+    # listdisplay设置要显示在列表中的字段（id字段是Django模型的默认主键）
+    list_display = ('id', 'createTime', 'actualNickName', 'nickName', 'text')
+    # list_per_page设置每页显示多少条记录，默认是100条
+    list_per_page = 5
+    # ordering设置默认排序字段，负号表示降序排序
+    ordering = ('-createTime',)
+    # 操作项功能显示位置设置，两个都为True则顶部和底部都显示
+    actions_on_top = True
+    actions_on_bottom = True
+    # 操作项功能显示选中项的数目
+    actions_selection_counter = True
+    # 字段为空值显示的内容
+    empty_value_display = ' -空白- '
+    # list_editable 设置默认可编辑字段
+    list_editable = []
+    # fk_fields 设置显示外键字段
+    # fk_fields = ()
+    # 过滤器功能及能过滤的字段
+    list_filter = ('actualNickName', 'nickName')
+    # 搜索功能及能实现搜索的字段
+    search_fields = ('actualNickName', 'nickName', 'text')
+
+
+admin.register(Msg)

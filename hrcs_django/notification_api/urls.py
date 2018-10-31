8042b6bf7d0a51861084952570084a9191dfc73d
gaojinhua <26028188@qq.com>
2018-10-31 09:10:54 +0800

    1031

--- hrcs_django/notification_api/urls.py
+++ hrcs_django/notification_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_five/', views.get_five, name='get_five'),
 ]
--- hrcs_django/notification_api/views.py
+++ hrcs_django/notification_api/views.py
@@ -12,6 +12,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_path)
 django.setup()
 # Create your views here.
 from django.http import HttpResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.serializers import serialize
 import json
 from msg2db.models import Msg
 
@@ -19,4 +21,16 @@ from msg2db.models import Msg
 def index(request):
     msg = Msg.objects.last()
     json_data = {"id": msg.id, "actualNickName": msg.actualNickName, "nickName": msg.nickName, "text": msg.text}
+    return HttpResponse(json.dumps(json_data), content_type="application/json")
+
+
+class LazyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Msg):
+            return str(obj)
+        return super().default(obj)
+
+
+def get_five(request):
+    json_data = serialize('json', Msg.objects.all()[:5], cls=LazyEncoder)
     return HttpResponse(json.dumps(json_data), content_type="application/json")

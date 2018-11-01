bdc0ea4395e576c3b6ad62d0832f61276a524c26
gaojinhua <26028188@qq.com>
2018-11-01 21:24:29 +0800

    1101

---
+++ pachong/test.py
@@ -0,0 +1,17 @@
+import xlrd
+
+xlsfile = 'loupanzidian.xls'
+book = xlrd.open_workbook(xlsfile)
+sheet_name = book.sheet_names()
+print(sheet_name)
+sheet = book.sheet_by_index(0)
+nrows = sheet.nrows
+ncols = sheet.ncols
+print(nrows)
+print(ncols)
+list = []
+for i in range(0, nrows):
+    value = sheet.cell(i, 0)
+    list.append(value)
+
+print(list.__str__().replace("text:", ""))
